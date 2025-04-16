a8072fac1088122e87d53744e35cb2fa5260a8b4
TeKaSs <98315145+Tekass13@users.noreply.github.com>
2025-04-16 19:59:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -62,13 +62,12 @@
           <h1> <?php echo $SiteName ?> </h1>
           <p class="mt-5 text-white-75">
             <?php echo $description ?>
-</body>
-</p>
-</div>
-</div>
-</div>
-</div>
-<section class="container servers">
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <section class="container servers">
   <hgroup class="text-center pb-3">
     <h2><?php echo $TitreModuleBox ?></h2>
     <p class="mt-4"><?php echo $DescritpionModuleBox ?>
@@ -180,6 +179,8 @@
   </div>
   </div>
 </footer>
+</body>
+
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -192,6 +193,5 @@
     window.history.replaceState(null, '', '/');
   }
 </script>
-</body>
 
 </html>
