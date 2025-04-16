906804d023f158a33d1485b9dc21fc70946347ba
TeKaSs <98315145+Tekass13@users.noreply.github.com>
2025-04-16 20:15:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 
   <script src="./assets/js/script.js"></script>
   <link rel="apple-touch-icon" sizes="57x57" href="/assets/favicon.png" />
-  <link rel="shortcut icon" href="<?php echo $favicon ?> type="image/x-icon" />
+  <link rel="shortcut icon" href="<?php echo $favicon ?>" type="image/x-icon" />
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
   <link rel="stylesheet" href="assets/css/style.css" />
