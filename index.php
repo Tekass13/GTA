545443385df59c790efd76035f7a86ad29bee70f
TeKaSs <98315145+Tekass13@users.noreply.github.com>
2025-04-16 20:14:38 +0200

    Update index.php

--- index.php
+++ index.php
@@ -18,9 +18,9 @@
   <meta name="revisit-after" content="2 days" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <script src="assets/js/script.js"></script>
+  <script src="./assets/js/script.js"></script>
   <link rel="apple-touch-icon" sizes="57x57" href="/assets/favicon.png" />
-  <link rel="shortcut icon" href="<?php echo $favicon ?>" type="image/x-icon" />
+  <link rel="shortcut icon" href="<?php echo $favicon ?> type="image/x-icon" />
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
   <link rel="stylesheet" href="assets/css/style.css" />
@@ -184,14 +184,14 @@
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<script src="assets/js/check_server.js"></script>
-<script src="assets/js/main.js"></script>
-<script src="assets/js/frontend-opt.js" type="text/javascript"></script>
+<script src="./assets/js/check_server.js"></script>
+<script src="./assets/js/main.js"></script>
+<script src="./assets/js/frontend-opt.js" type="text/javascript"></script>
 
-<!-- <script>
+<script>
   if (window.location.pathname === '/index.html') {
     window.history.replaceState(null, '', '/');
   }
-</script> -->
+</script>
 
 </html>
